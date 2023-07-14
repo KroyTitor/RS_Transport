353ea2c94bb3c83cdf3ca3935936ce47c5e27f80
KroyTitor <104220236+KroyTitor@users.noreply.github.com>
2023-07-14 12:33:25 +0200

    New page added

---
+++ java.js
@@ -0,0 +1,11 @@
+/*let images = ["IMG/sprinter-truck.jpeg", "IMG/sprinter-truck2.jpg", "IMG/sprinter-truck3.jpg", "IMG/sprinter-truck4.jpg", "IMG/sprinter-truck5.jpg"];*/
+let images = [/*"IMG/sprinter-truck.jpg",*/"IMG/sprinter-truck2.jpg", "IMG/sprinter-truck3.jpg", "IMG/sprinter-truck4.jpg"/*, "IMG/sprinter-truck5.jpg"*/];
+let currentIndex = 0;
+
+function switchImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  let imageElement = document.getElementById("image");
+  imageElement.src = images[currentIndex];
+}
+
+setInterval(switchImage, 5000);

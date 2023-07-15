4c79e83b2b32c3b6c3c6bd403fcd3aabae7efed2
KroyTitor <104220236+KroyTitor@users.noreply.github.com>
2023-07-15 13:25:07 +0200

    Addition to js file, transistion animation added to page switching

--- java.js
+++ java.js
@@ -6,5 +6,26 @@ function switchImage() {
   let imageElement = document.getElementById("image");
   imageElement.src = images[currentIndex];
 }
-
 setInterval(switchImage, 5000);
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all the anchor tags
+  const links = document.querySelectorAll('a');
+
+  // Attach a click event listener to each anchor tag
+  links.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      // Add the fade-out class to the content section
+      const content = document.querySelector('.content');
+      content.classList.add('fade-out');
+
+      // Wait for the fade-out animation to finish
+      setTimeout(function() {
+        // Navigate to the new page
+        window.location.href = link.href;
+      }, 300); // Adjust the timeout to match the duration of the fade-out animation
+    });
+  });
+});
